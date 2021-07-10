3a796e44815dd5fc8d21a6883e82eb4a6d90e2b5
emreeserege <emreeser535353@gmail.com>
2021-07-10 16:32:27 +0300

    some files deleted

--- cloud-gateway/src/main/java/com/HotelReservation/cloud/gateway/FallBackMethodController.java
+++ cloud-gateway/src/main/java/com/HotelReservation/cloud/gateway/FallBackMethodController.java
@@ -12,12 +12,6 @@ public class FallBackMethodController {
                 " Please try again later";
     }
 
-    @GetMapping("/hotelServiceFallBack")
-    public String hotelServiceFallBackMethod(){
-        return "Hotel service is taking longer than expected."+
-                " Please try again later";
-    }
-
     @GetMapping("/bookingServiceFallBack")
     public String bookingServiceFallBackMethod(){
         return "Booking service is taking longer than expected."+
